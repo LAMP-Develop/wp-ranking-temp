68afda8c1bec7cdf6e031284fb4e9b8cd8806964
ShunyaSuzuki <anyushu2017@gmail.com>
2020-05-27 14:31:13 +0900

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -25,14 +25,14 @@ $(function ($) {
   }
 
   // スムーススクロール
-  $('a[href^="#"]').on("click", function () {
-    var speed = 500;
-    var href = $(this).attr("href");
-    var target = $(href == "#" || href == "" ? "html" : href);
-    var position = target.offset().top - 100;
-    $("html, body").animate({ scrollTop: position }, speed, "swing");
-    return false;
-  });
+  // $('a[href^="#"]').on("click", function () {
+  //   var speed = 500;
+  //   var href = $(this).attr("href");
+  //   var target = $(href == "#" || href == "" ? "html" : href);
+  //   var position = target.offset().top - 100;
+  //   $("html, body").animate({ scrollTop: position }, speed, "swing");
+  //   return false;
+  // });
 
   // スクロールイベント
   // $(window).on("scroll", function () {
--- header.php
+++ header.php
@@ -6,6 +6,9 @@ $wp_url = get_template_directory_uri().'/dist/'; ?>
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width,initial-scale=1.0">
+<?php if (get_post_type() === 'merchant'): ?>
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css">
+<?php endif; ?>
 <?php wp_head(); ?>
 <?php if (!is_user_logged_in()): ?>
 <!-- ここにGAトラッキングタグ -->
