5efa437dae6d0f4fdc5386c0a201ac416e0997c4
ShunyaSuzuki <anyushu2017@gmail.com>
2020-06-02 19:09:10 +0900

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,10 +4,9 @@ import "../sass/app.scss";
 // JSインポート
 import "bootstrap";
 
-$(function ($) {
+$(window).on('load resize', function() {
   let w = $(window).width();
   let md = 991.98;
-
   if (w <= md) {
     // 画像src切り替え
     $(".img-switch").each(function () {
@@ -22,7 +21,42 @@ $(function ($) {
         $(this).attr("srcset", result);
       }
     });
+  } else {
+    // 画像src切り替え
+    $(".img-switch").each(function () {
+      $(this).attr("src", $(this).attr("src").replace("_sp", "_pc"));
+      let srcset = $(this).attr("srcset");
+      if (srcset != null) {
+        let result = srcset.replace("_sp", "_pc");
+        while (result !== srcset) {
+          srcset = srcset.replace("_sp", "_pc");
+          result = result.replace("_sp", "_pc");
+        }
+        $(this).attr("srcset", result);
+      }
+    });
   }
+});
+
+// $(function ($) {
+  // let w = $(window).width();
+  // let md = 991.98;
+
+  // if (w <= md) {
+  //   // 画像src切り替え
+  //   $(".img-switch").each(function () {
+  //     $(this).attr("src", $(this).attr("src").replace("_pc", "_sp"));
+  //     let srcset = $(this).attr("srcset");
+  //     if (srcset != null) {
+  //       let result = srcset.replace("_pc", "_sp");
+  //       while (result !== srcset) {
+  //         srcset = srcset.replace("_pc", "_sp");
+  //         result = result.replace("_pc", "_sp");
+  //       }
+  //       $(this).attr("srcset", result);
+  //     }
+  //   });
+  // }
 
   // スムーススクロール
   // $('a[href^="#"]').on("click", function () {
@@ -38,4 +72,4 @@ $(function ($) {
   // $(window).on("scroll", function () {
   //   let s = $(window).scrollTop();
   // });
-});
+// });
